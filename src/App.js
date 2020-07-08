b6e1e359fdc46d66d7d8b1855fd76feb9437ed30
Mark Bean <mark.bean0302@gmail.com>
2020-07-07 22:52:22 -0400

    Second Day Completion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
+import particlesOptions from "./particlesOptions";
 import Particles from 'react-particles-js';
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo";
 import Rank from "./Components/Rank/Rank";
+import Clarifai from 'clarifai';
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
 import './App.css';
 
-const particlesOptions = {
-    particles: {
-        number: {
-            value: 30,
-            density: {
-                enable: true,
-                value_area: 225
-            }
-        }
-    }
-}
+const app = new Clarifai.App({
+    apiKey: '175962bb26b54a46b1b2df6f1ae019c1'
+});
+
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: ''
+        }
+    };
+
+    onInputChange = (event) => {
+        console.log(event.target.value);
+    };
+
+    onButtonSubmit = () => {
+        app.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+            .then(generalModel => {
+                return generalModel.predict("http://doc.angus.ai/_images/aurelien.jpg");
+            })
+            .then(response => {
+                let concepts = response['outputs'][0]['data']['concepts'];
+                console.log(response);
+            })
+    };
+
     render() {
         return (
             <div className="App">
@@ -29,7 +46,10 @@ class App extends Component {
                 <Navigation />
                 <Logo/>
                 <Rank />
-                <ImageLinkForm />
+                <ImageLinkForm
+                    onInputChange={this.onInputChange}
+                    onButtonSubmit={this.onButtonSubmit}
+                />
                 {/*<FaceRecognition />*/}
         </div>
         );
--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
             <p className='f3'>
@@ -9,8 +9,18 @@ const ImageLinkForm = () => {
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type="text"/>
-                    <button className='w-25 grow f4 link ph3 pv2 dib white br-pill ba b--white' style={{backgroundColor: '#00d5ff'}}>Detect</button>
+                    <input
+                        className='f4 pa2 w-70 center'
+                        type="text"
+                        onChange={onInputChange}
+                    />
+                    <button
+                        className='w-25 grow f4 link ph3 pv2 dib white br-pill ba b--white'
+                        style={{backgroundColor: '#00d5ff'}}
+                        onClick={onButtonSubmit}
+                    >
+                        Detect
+                    </button>
                 </div>
             </div>
         </div>
