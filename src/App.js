7bfd4c4f93691fdadba6d21ac348d05a6e2c762c
Mark Bean <mark.bean0302@gmail.com>
2020-07-10 00:20:55 -0400

    Fourth Day Progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import particlesOptions from "./particlesOptions";
 import Particles from 'react-particles-js';
+import SignIn from "./Components/SignIn/SignIn";
+import Register from "./Components/Register/Register";
+import Clarifai from 'clarifai';
+import FaceRecognition from "./Components/FaceRecognition/FaceRecognition";
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo";
-import Rank from "./Components/Rank/Rank";
-import Clarifai from 'clarifai';
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
-import FaceRecognition from "./Components/FaceRecognition/FaceRecognition";
+import Rank from "./Components/Rank/Rank";
 import './App.css';
 
 const app = new Clarifai.App({
@@ -20,7 +22,9 @@ class App extends Component {
         this.state = {
             input: '',
             imageUrl: '',
-            box: {}
+            box: {},
+            route: 'signIn',
+            isSignedIn: false
         }
     };
 
@@ -29,8 +33,17 @@ class App extends Component {
         const image = document.getElementById('inputImage');
         const width = Number(image.width);
         const height = Number(image.height);
-        console.log(width, height);
-}
+        return {
+            leftCol: clarifaiFace.left_col * width,
+            topRow: clarifaiFace.top_row * height,
+            rightCol: width - (clarifaiFace.right_col * width),
+            bottomRow: height - (clarifaiFace.bottom_row * height)
+        }
+    }
+
+    displayFaceBox = (box) => {
+        this.setState({ box: box })
+    }
 
     onInputChange = (event) => {
         this.setState({input: event.target.value})
@@ -39,30 +52,50 @@ class App extends Component {
     onButtonSubmit = ({ imageUrl }) => {
         this.setState({imageUrl: this.state.input})
         app.models.predict("a403429f2ddf4b49b307e318f00e528b", this.state.input)
-            .then(response => this.calculateFaceLocation(response))
+            .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
             .catch(err => console.log("Something went wrong...", err))
     };
 
+    onRouteChange = (route) => {
+        if (route === 'signOut') {
+            this.setState({isSignedIn: false})
+        } else if (route === 'home') {
+            this.setState({isSignedIn: true})
+        }
+        this.setState({route: route});
+    }
+
     render() {
+        const { isSignedIn, imageUrl, route, box } = this.state;
         return (
             <div className="App">
                 <Particles
                     className='particles'
                     params={particlesOptions}
                 />
-                <Navigation />
-                <Logo/>
-                <Rank />
-                <ImageLinkForm
-                    onInputChange={this.onInputChange}
-                    onButtonSubmit={this.onButtonSubmit}
-                />
-                <FaceRecognition
-                    imageUrl={this.state.imageUrl}
-                />
+                <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+                { route === 'home'
+                    ? <div>
+                        <Logo/>
+                        <Rank />
+                        <ImageLinkForm
+                            onInputChange={this.onInputChange}
+                            onButtonSubmit={this.onButtonSubmit}
+                        />
+                        <FaceRecognition
+                            imageUrl={imageUrl}
+                            box={box}
+                        />
+                    </div>
+                    : (
+                        route === 'signIn'
+                        ? <SignIn onRouteChange={this.onRouteChange} />
+                        : <Register onRouteChange={this.onRouteChange} />
+                    )
+                }
         </div>
         );
     };
-};
+}
 
 export default App;
--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,20 @@
 import React from "react";
+import "./FaceRecognition.css"
 
-const FaceRecognition = ({ imageUrl }) => {
+
+const FaceRecognition = ({ imageUrl, box }) => {
     return (
-        <div className='center ma'>
-            <div className='mt2'>
-                <img id="inputImage" src={imageUrl} alt="facial-recognition" width='500px' height='auto'/>
+        <div>
+            <div className='center mt4'>
+                <div className='absolute-correction absolute mt2'>
+                    <img id="inputImage" src={imageUrl} alt=""
+                         width='500px'
+                         height='auto'
+                    />
+                    <div className='bounding-box'
+                         style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}
+                    />
+                </div>
             </div>
         </div>
     );
